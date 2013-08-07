bd3c0b924b27fbb67fd3a16a47d53969f03c5af4
Charles Max Wood <chuck@devchat.tv>
2013-08-07 22:34:04 +0000

    Initial Rails Setup + RSpec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlShortener::Application.config.secret_key_base = 'b9eb6c6afe97f1c3cc54dbb013b792a3cdd7cec5a2dcc301550008c06d48b64eb4aa7bc3bfdfaa102833c9bb32854bad123dc5738c3ed1f795eae8dad9a4d288'
