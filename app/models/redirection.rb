03b101b60383bac5c52d2b3ea944b93641c56912
Charles Max Wood <cmaxw@devchat.tv>
2024-04-22 12:17:35 -0600

    Successfully creates redirection

--- app/models/redirection.rb
+++ app/models/redirection.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: redirections
+#
+#  id              :bigint           not null, primary key
+#  destination_url :string
+#  source_path     :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class Redirection < ApplicationRecord
+  validates :source_path, presence: true, uniqueness: true, length: { in: 2..8 }
+  validates :destination_url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp }
 end
--- test/models/redirection_test.rb
+++ test/models/redirection_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: redirections
+#
+#  id              :bigint           not null, primary key
+#  destination_url :string
+#  source_path     :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require "test_helper"
 
 class RedirectionTest < ActiveSupport::TestCase
